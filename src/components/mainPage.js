47cd4565aaa8cf092c1e597f52c2636e7f423b8f
Maxim <maxrap7878@mail.ru>
2018-09-01 00:06:23 +0300

    animation trying

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -3,16 +3,26 @@ import "./mainPage.css"
 
 
 class MainPage extends Component {
+    constructor(props){
+        super(props);
+    }
+
+
     render() {
+        const artAnimation = () => {
+           const music = document.getElementById('music');
+           music.style.width = '100%';
+        };
+
         return (
             <div className="main-page-container" >
-                <div className="menu-container" style={{zIndex: -2}} >
+                <div className="menu-container" style={{zIndex: 1}} >
                     <h2>PASIKA</h2>
                     <p>MAGAZINE</p>
                     <ul className='menu'>
-                        <li><a >ART</a></li>
-                        <li><a >FASHION</a></li>
-                        <li><a >MUSIC</a></li>
+                        <li onClick={artAnimation}><a>ART</a></li>
+                        <li><a>FASHION</a></li>
+                        <li><a>MUSIC</a></li>
                     </ul>
                 </div>
                 <div className="additional-menu" style={{zIndex: -1}}>
@@ -24,8 +34,12 @@ class MainPage extends Component {
                     <div className="fashion-container" style={{zIndex: -4}}>
                     </div>
                 <div className="curtains-container"  style={{zIndex: -3}} >
-                    <div className="music-container" style={{zIndex: -3}}> </div>
+                    <div className="music-container" id="music" style={{zIndex: -3}}> </div>
                     <div className="art-container" style={{zIndex: -3}}> </div>
+                </div>
+                <div className="social-container">
+                    <a href='https://www.instagram.com/pasikamagazine/'><img className="insta" src={require(".././icons/INSTA.svg")} style={{zIndex: 1}}/></a>
+                    <a><img className="facebook" src={require(".././icons/FACE.svg")} style={{zIndex: 1}} /></a>
                 </div>
 
 
